26232ce6f7a11302efbdacfee6b83683eea40cce
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-10-17 13:58:57 -0700

    unit testing prep

---
+++ bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'config/config.php';
+    require_once 'includes/HTTPRequest.php';
+    require_once 'includes/HTTPResponse.php';
+    require 'vendor/autoload.php';   
+    require_once 'includes/appRouter.php';
