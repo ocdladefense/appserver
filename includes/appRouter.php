e6303b3e593ef91e099fe76c434bce1dfae8f766
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-10-18 14:41:47 -0700

    application should have state

--- includes/appRouter.php
+++ includes/appRouter.php
@@ -4,120 +4,139 @@ class AppRouter
     private $completeRequestedPath = "";
     private $resourceString = "";
     private $pathToRequestedResource = "";
+    
+    private $filesIncluded = array();
     private $arguments = array();
     private $allRoutes = array();
+    private $modules = array();
 
 
-    public function __construct($path)
-    {
+    public function __construct($path){
         $this->completeRequestedPath = $path;
         $this->initializeRoutes();
-        $this->parsePath();
+        $this->parsePath();   
     }
 
-    public function parsePath()
-    {
-        if(strpos($this->completeRequestedPath,"/") == 0)
-        {
+    public function parsePath(){
+        if(strpos($this->completeRequestedPath,"/") == 0){
             $this->completeRequestedPath = substr($this->completeRequestedPath,1);
         }
         //isolate the resource string from the requested path
         $parts = explode("?", $this->completeRequestedPath);
         $this->resourceString = $parts[0];
         $parts = explode("/", $this->resourceString);
+        //remove the base path
+        array_shift($parts);
         //match the completeRequestedPath to a path of a resource
         $this->pathToRequestedResource = array_shift($parts);
         //subtract the matching path you are left with the args
         $this->arguments = $parts;
     }
-
-    public function getArg($index)
-    {
-        return $this->arguments[$index];
+    public function processRoute(){
+        $route = $this->getActiveRoute($this->modules);
+        $this->requireRouteFiles($route);
+        $this->setHeaderContentType($route);
+        return $this->callCallbackFunction($route);
     }
-
-    public function getArgs()
-    {
-        return $this->arguments;
+    public function initializeRoutes(){
+        $this->modules = $this->getModules();
+        $this->loadModules($this->modules);
+        $this->allRoutes = getAllRoutesForModule($this->modules);
     }
-
-    public function processRoute()
-    {
-        //allRoutes is all possible routes "collected" from the names of the files in modules minus the "Mod"
-        if(!array_key_exists($this->pathToRequestedResource,$this->allRoutes))
-        {
-            throw new exception($this->pathToRequestedResource." could not be found");
-        }
-        $route = $this->allRoutes[$this->pathToRequestedResource];
-
-        //loop through files to include the files in the route
-        $filesIncluded = array();
-        foreach($route["files"] as $file)
-        {
-            $file = "../modules/{$route['module']}/".$file;
-            require($file);
-            array_push($filesIncluded,$file);
-        }
-        if($route["content-type"] == "json")
-        {
-            header("Content-type: application/json; charset=utf-8");
-        }
-        if($route["method"] == "post")
-        {
-            $entityBody = file_get_contents('php://input');
-            return call_user_func_array($route["callback"],array($entityBody));
-            
-        }
-        else
-        {
-            return call_user_func_array($route["callback"],$this->getArgs());
-        }
-    }
-
-    public function initializeRoutes()
-    {
-        //loop through the files in the modules folder in the directory
-        //for each file, isolate the module name and add it to the "$modules" array
-        //use the "include" function to run the script which will make the functions available
+    public function getModules(){
         $previous = getcwd();
         chdir("../modules");
         $modules = array();
 
         $files = scandir(".");
-        foreach($files as $dir)  
-        {
+
+        foreach($files as $dir)  {
             if(!is_dir($dir) || $dir == ".." || $dir == ".")
             continue;
             $modules[] = $dir;
-            require($dir."/module.php");
         }
         chdir($previous);
-        //loop through the "$modules" array
-        //for each module name in the "$modules" array, concatinate the module name with ".ModRoutes" and assign that value to the "$routes" variable
-        //use the "array_merge" function to merge the "$allRoutes" array and the "$routes" variable (as an array) and assign that value to the "$allRoutes" variable
-        $this->allRoutes = array();
-        foreach($modules as $mod)
-        {
+        return $modules;
+    }
+    public function loadModules($modules){
+        $previous = getcwd();
+        chdir("../modules");
+
+        $files = scandir(".");
+
+        foreach($modules as $mod)  {
+            require($mod."/module.php");
+        }
+        chdir($previous);
+    }
+    public function getActiveRoute($modules){
+
+        if(!array_key_exists($this->pathToRequestedResource,$this->allRoutes)){
+            throw new exception($this->pathToRequestedResource." could not be found");
+        }
+        return $this->allRoutes[$this->pathToRequestedResource];
+    }
+    public function getAllRoutesForModule($modules){
+        foreach($this->modules as $mod){
             $functionName = $mod . "ModRoutes";
             $routes = call_user_func($functionName);
-            foreach($routes as &$route)
-            {
+            foreach($routes as &$route){
                 $route["module"] = $mod;
                 $route["method"] = !empty($route["method"])?$route["method"]:"get";
                 $route["content-type"] = !empty($route["content-type"])?$route["content-type"]:"html";
             }
-            $this->allRoutes = array_merge($routes, $this->allRoutes);
+            return array_merge($routes, $this->allRoutes);
         }
     }
+    public function requireRouteFiles($route){
+        foreach($route["files"] as $file){
+            $file = "../modules/{$route['module']}/".$file;
+            require($file);
+            array_push($this->filesIncluded,$file);
+        }
+    }
+    public function setHeaderContentType($route){
+        if($route["content-type"] == "json"){
+            header("Content-type: application/json; charset=utf-8");
+        }
+    }
+    public function callCallbackFunction($route){
+        if($route["method"] == "post"){
+            $entityBody = file_get_contents('php://input');
+            return call_user_func_array($route["callback"],array($entityBody));   
+        }
+        else{
+            return call_user_func_array($route["callback"],$this->getArgs());
+        }
+    }
+    public function getArg($index){
+        return $this->arguments[$index];
+    }
+    public function getArgs(){
+        return $this->arguments;
+    }
     
+
+
+
+    //Functions that return state data for testing purposes
     public function getcompleteRequestedPath(){
         return $this->completeRequestedPath;
     }
-    public function listAllPaths(){
+    public function getPaths(){
 
-        return
-        "completeRequestedPath ====".$this->completeRequestedPath."*********************************************".
-        "resourceString ====".$this->resourceString."*********************************************".
-        "pathToRequestedResource ====".$this->pathToRequestedResource."*********************************************";
+        echo "\n"."\n".
+        "completeRequestedPath ====".$this->completeRequestedPath."*********************************************"."\n"."\n".
+        "resourceString ====".$this->resourceString."*******************************************"."\n"."\n".
+        "pathToRequestedResource ====".$this->pathToRequestedResource."*************************"."\n"."\n";
+    }
+    public function getRoutes(){
+        print_r($this->allRoutes);
+    }
+    public function getArguments(){
+        print_r ($this->arguments);
+    }
+    public function getFilesIncluded(){
+        print_r($this->filesIncluded);
     }
 }
--- tests/unit-tests.php
+++ tests/unit-tests.php
@@ -1,12 +1,36 @@
 <?php
 require '../bootstrap.php';
 
-function isFinding(){
-    return "testing page is found";
-}
 
+//echo isRequestedPath();
+//echo displayAllPaths();
+//displayAllFilesIncluded();
+//displayAllArguments();
+//displayAllRoutes();
+testValidPath();
+
+
+function testValidPath(){
+    $contactId = "999";
+    $validPath = "/customer-profile-id/{$contactId}";
+    $router = new AppRouter($validPath);
+
+    $mods = $router->getModules();
+
+
+    if(!is_dir("../modules/salesforce")){
+        throw new Exception("Not a directory");
+    }
+
+    if(!in_array("salesforce",$mods)){
+        throw new Exception("should have loaded salesforce module");
+    }
+
+
+    
+}
 function isRequestedPath(){
-    $testPath = "/foobar";
+    $testPath = "part1/part2/part3/part4";
     $router = new AppRouter($testPath);
     $requestedPath = $router->getRequestedPath();
 
@@ -14,6 +38,25 @@ function isRequestedPath(){
         return "true, path is ".$requestedPath;
         return "false, path is ".$requestedPath;
 }
+function displayAllPaths(){
+    $testPath = "part1/part2/part3/part4?SOME-PARAMTER-STRING";
+    $router = new AppRouter($testPath);
+    return $router->listAllPaths();
+}
+function displayAllRoutes(){
+    $testPath = "part1/part2/part3/part4?SOME-PARAMTER-STRING";
+    $router = new AppRouter($testPath);
+    return $router->listAllRoutes();
+}
+function displayAllArguments(){
+    $testPath = "part1/part2/part3/part4?SOME-PARAMTER-STRING";
+    $router = new AppRouter($testPath);
+    return $router->listAllArguments();
+}
+function displayAllFilesIncluded(){
+    $testPath = "part1/part2/part3/part4?SOME-PARAMTER-STRING";
+    $router = new AppRouter($testPath);
+    return $router->listAllFilesIncluded();
+}
 
-echo isFinding();
-echo isRequestedPath();
+
