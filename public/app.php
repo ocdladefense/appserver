45a2c05496c14deb85bb2f3f0ceb37595e8f6a25
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-10-22 15:06:45 -0700

    pass the path to the appserver via the new method

--- public/app.php
+++ public/app.php
@@ -4,7 +4,9 @@ require '../bootstrap.php';
 session_start();
 
 
-$router = new AppRouter($_SERVER["REQUEST_URI"]);
+//$router = new AppRouter($_SERVER["REQUEST_URI"]);
+$router = new AppRouter();
+$router->recievePath($_SERVER['REQUEST_URI']);
 $responseBody = $router->processRoute();
 
 print ($responseBody);
