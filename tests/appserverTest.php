8b1cbd8de22e24366dd339b534fe71cd2e759a4f
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-10-22 15:05:55 -0700

    unit tests for the appserver

---
+++ tests/appserverTest.php
@@ -0,0 +1,51 @@
+<?php
+// Unit tests for our API Integration.
+
+//EXECUTION OF TESTS IS './vendor/bin/phpunit  tests'
+
+declare(strict_types=1);
+include "vendor/autoload.php";
+require "bootstrap.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class AppserverTest extends TestCase
+{
+    private $validPathWithArgs = "/customer-profile-id?999";
+    private $contactId = "999";
+    private $completeRequestedResource = "customer-profile-id?999";
+    private $resourceString = "customer-profile-id";
+    private $pathToRequestedResource = null;
+    
+    public function testParsePathResults(): void{
+        $router = new AppRouter();
+        $router->parsePath($this->validPathWithArgs);
+
+        $this->assertEquals($this->completeRequestedResource, $router->getCompleteRequestedPath(), "Paths are not Equal");
+        $this->assertEquals($this->resourceString, $router->getResourceString(), "Paths are not Equal");
+        $this->assertEquals($this->resourceString, $router->getPathToRequestedResource(), "Paths are not Equal");
+        $this->assertEquals($this->contactId, $router->getArgs()[0], "getArgs()[0] is not equal to the given argument");
+        $this->assertEquals($this->contactId, $router->getArg(0), "getArgs(0) is not equal to the given argument");
+    } 
+    public function testGetModules(): void{
+        $router = new AppRouter();
+        $expectedModulesArray = ['authorizeNet','salesforce'];
+
+        $this->assertEquals('authorizeNet', $router->getModules()[0], "The string 'authorizeNet' is not the element at the index of 0");
+        $this->assertEquals('salesforce', $router->getModules()[1], "The string 'salesforce' is not the element at the index of 1");
+        $this->assertEquals($expectedModulesArray, $router->getModules(), "The arrays are not the same");
+    }
+    public function testLoadModules(): void{
+        $router = new AppRouter();
+        $expectedModulesArray = ['authorizeNet','salesforce'];
+
+        //require "modules/authorizeNet/module.php";
+
+        $router->LoadModules($expectedModulesArray);
+        $this->assertTrue(function_exists("chargeCard"));
+    }
+
+
+}
+
+
