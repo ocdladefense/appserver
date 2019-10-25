ffca88e18a817e05f5fcdfb9d9b2213a618fce96
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-10-25 10:02:59 -0700

    a new class for module initilization

---
+++ includes/appModules.php
@@ -0,0 +1,39 @@
+<?php
+class AppModules{
+    private static $PATH_TO_MODULES = __DIR__ ."/../modules";
+    private $modules = array();
+
+    public function __construct(){}
+
+
+    //Find all of the modules installed in the modules directory and add them to the "this->modules" array.
+    public function discoverFileSystemModules(){
+        $previous = getcwd();
+        chdir(self::$PATH_TO_MODULES);
+        $modules = array();
+
+        $files = scandir(".");
+
+        foreach($files as $dir)  {
+            if(!is_dir($dir) || $dir == ".." || $dir == ".")
+            continue;
+            $modules[] = $dir;
+        }
+        chdir($previous);
+        return $modules;
+    }
+
+    //Scan each module directory in the "this->modules" array and require its "module.php" file.
+    public function loadFileSystemModules($modules){
+        $this->modules = $modules;
+        $previous = getcwd();
+        chdir(self::$PATH_TO_MODULES);
+
+        $files = scandir(".");
+
+        foreach($modules as $mod)  {
+            require_once($mod."/module.php");
+        }
+        chdir($previous);
+    }
+}
--- includes/appRouter.php
+++ includes/appRouter.php
@@ -3,7 +3,7 @@ class AppRouter
 {
     private static $DEFAULT_HTTP_METHOD = "get";
     private static $DEFAULT_CONTENT_TYPE = "html";
-    private static $PATH_TO_MODULES = __DIR__ ."/../modules";
+    
     
 
     private $completeRequestedPath = "";
@@ -13,19 +13,17 @@ class AppRouter
     private $filesIncluded = array();
     private $arguments = array();
     private $allRoutes = array();
-    private $modules = array();
+    
 
     public function __construct(){}
 
-    public function recievePath($path){
-        $router = new self();
-        $this->parsePath($path);
-        $this->initializeRoutes();  
+    public function setPath($path){
+        $this->completeRequestedPath = $path;
     }
 
-    public function parsePath($path){
-        $this->completeRequestedPath = $path;
-
+    public function parsePath($path = null){
+        if(!$path == null)
+            $this->completeRequestedPath = $path;
         //Remove prevailing slash
         if(strpos($this->completeRequestedPath,"/") == 0){
             $this->completeRequestedPath = substr($this->completeRequestedPath,1);
@@ -41,11 +39,6 @@ class AppRouter
         //match the completeRequestedPath to a path of a resource
         $this->pathToRequestedResource = array_shift($parts);
     }
-    public function initializeRoutes(){
-        $this->modules = $this->getModules();
-        $this->loadModules($this->modules);
-        $this->allRoutes = $this->setRouteDefaults($this->modules);
-    }
     public function processRoute(){
         $route = $this->getActiveRoute($this->modules);
         $this->requireRouteFiles($route);
@@ -56,32 +49,8 @@ class AppRouter
 
 
     
-    public function getModules(){
-        $previous = getcwd();
-        chdir(self::$PATH_TO_MODULES);
-        $modules = array();
-
-        $files = scandir(".");
-
-        foreach($files as $dir)  {
-            if(!is_dir($dir) || $dir == ".." || $dir == ".")
-            continue;
-            $modules[] = $dir;
-        }
-        chdir($previous);
-        return $modules;
-    }
-    public function loadModules($modules){
-        $previous = getcwd();
-        chdir(self::$PATH_TO_MODULES);
-
-        $files = scandir(".");
-
-        foreach($modules as $mod)  {
-            require($mod."/module.php");
-        }
-        chdir($previous);
-    }
+    
+    
     public function getActiveRoute($modules){
 
         if(!array_key_exists($this->pathToRequestedResource,$this->allRoutes)){
@@ -90,20 +59,6 @@ class AppRouter
         return $this->allRoutes[$this->pathToRequestedResource];
     }
 
-    //Returns all of the routes from all of the modules
-    //setting default values for all routes from all modules
-    public function setRouteDefaults($modules){
-        foreach($this->modules as $mod){
-            $routeFunction = $mod . "ModRoutes";
-            $routes = call_user_func($routeFunction);
-            foreach($routes as &$route){
-                $route["module"] = $mod;
-                $route["method"] = !empty($route["method"])?$route["method"]:self::$DEFAULT_HTTP_METHOD;
-                $route["content-type"] = !empty($route["content-type"])?$route["content-type"]:self::$DEFAULT_CONTENT_TYPE;
-            }
-            return array_merge($routes, $this->allRoutes);
-        }
-    }
     public function requireRouteFiles($route){
         foreach($route["files"] as $file){
             $file = "../modules/{$route['module']}/".$file;
@@ -142,8 +97,17 @@ class AppRouter
     public function getArgs(){
         return $this->arguments;
     }
-    public function getAllRoutes(){
-        //return $this->allRoutes();
-        //print_r ($this->allRoutes);
+    public function initRoutes(){
+        foreach($this->modules as $mod){
+            $routeFunction = $mod . "ModRoutes";
+            $routes = call_user_func($routeFunction);
+            foreach($routes as &$route){
+                $route["module"] = $mod;
+                $route["method"] = !empty($route["method"])?$route["method"]:self::$DEFAULT_HTTP_METHOD;
+                $route["content-type"] = !empty($route["content-type"])?$route["content-type"]:self::$DEFAULT_CONTENT_TYPE;
+            }
+               $this->allRoutes = array_merge($this->allRoutes,$routes);
+        }
+        return $this->allRoutes;
     }
 }
