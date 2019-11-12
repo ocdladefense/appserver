efafa8d657ca801201dfa5ef9fe9487f40720618
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-11-12 10:46:39 -0800

    requireRouteModules

--- includes/AppRouter.php
+++ includes/AppRouter.php
@@ -90,10 +90,19 @@ class AppRouter
     //if the route requires the use of methods from another module, require the module
     public function requireRouteModules($route){
         foreach($route["modules"] as $additionaModule){
-            $additionaModule = getPathToModules()."/{$route['module']}/module.php";
+            var_dump($route["modules"]);
+
+            $installedModules = scandir(getPathToModules());
+            
+            if(!in_array($additionaModule,$installedModules)){
+                throw new Exception("$additionaModule is required for the given route, but it is not installed in the modules directory");
+            }
+            
+            $additionalModuleFile = getPathToModules()."/$additionaModule/module.php";
             require_once($additionaModule);
             array_push($this->additionalModules,$additionaModule);
             print_r ($this->additionalModules);
+            var_dump($installedModules);
             exit;
         }
     } 
