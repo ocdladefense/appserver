65106d27419011bb29be12f62bb6fe77942eafcc
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-11-20 12:15:25 -0800

    add the request to the application state

--- public/app.php
+++ public/app.php
@@ -2,14 +2,25 @@
 require '../bootstrap.php';           
 
 session_start();
+
+$request = HTTPRequest::newFromEnvironment();
+// print_r($request->getHeaders());
+// exit;
+
 $application = new Application();
 $loader = new ModuleLoader();
 $application->setModuleLoader($loader);
-//$application->setFileSystem(new FileSystem());
-
-
+$application->setRequest($request);
 
 $router = new Router($application);
-$responseBody = $router->run($_SERVER['REQUEST_URI']);
-$router->sendHeaders();
-print_r ($responseBody);
+$responseBody = $router->run($application->getRequestHeader("Request-URI"));
+
+if($router->getHeaders()["Content-type"] == $application->getRequestHeader("Accept") || stringContains($application->getRequestHeader("Accept"),"*/*")){
+    $router->sendHeaders();
+}
+else{
+    throw new Exception("The content type of the requested resource '{$router->getHeaders()["Content-type"]}' does not match the accepted
+     content type '{$application->getRequestHeader("Accept")}', which is set by the requesting entity.  Exception thrown");
+}
+
+print $responseBody;
