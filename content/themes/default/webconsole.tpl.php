bb714ecdb5ad25ae37bb3057873a9a94b3646d56
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2019-11-29 21:18:37 -0800

    a ui object

--- content/themes/default/webconsole.tpl.php
+++ content/themes/default/webconsole.tpl.php
@@ -12,7 +12,7 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>Using modal cc form</title>
-		<link href="/examples/assets/css/ux.css" type="text/css" rel="stylesheet" />
+		<link href="/modules/webconsole/assets/css/ux.css" type="text/css" rel="stylesheet" />
 		<link href="/modules/webconsole/assets/css/materials.css" type="text/css" rel="stylesheet" />
 		<link href="/modules/webconsole/assets/css/keyboardManager.css" type="text/css" rel="stylesheet" />
 		<script type="text/javascript">
@@ -23,6 +23,8 @@
 
 		</script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src=/modules/webconsole/assets/components/materials.js>
+		</script>
 		<script src="//membertest.ocdla.org/sites/all/libraries/library/event.js">
 		</script>
 		<script src="//membertest.ocdla.org/sites/all/libraries/library/view-core.js">
@@ -33,9 +35,11 @@
 		</script>
 		<script src=/modules/webconsole/assets/user-routes.js>
 		</script>
-		<script src=/modules/webconsole/assets/viewLibrary.js>
+		<script src=/modules/webconsole/assets/ui.js>
 		</script>
-		<script src=/modules/webconsole/assets/components/materials.js>
+		<script src=/modules/webconsole/assets/modal.js>
+		</script>
+		<script src=/modules/webconsole/assets/viewLibrary.js>
 		</script>
 		<script src=/modules/webconsole/assets/fetch.js>
 		</script>
@@ -45,39 +49,31 @@
     	 <?php /* echo $scripts*/ ?> 
 	</head>
 	<body>
-	
-   
-    </div>
+
 		<div id="page">
-            
-			<div class="menu-left">&nbsp;</div>
-			
-			<div id="stage"><h1><?php echo $content  ?></h1></div>
-
 			<div id="header">
-			<div id="logo">AppServer GUI</div>
-			<div id="banner">&nbsp;
-			</div>
-		</div>
-		<h1>View or download event materials below</h1>
-    	<div class="container2" id = "dropDownContainer">
-        <select id = "chapterDropDown">
-        <option selected="selected" value = "Show All Chapters">Show All Chapters</option>
-        </select>
+				<div id="logo">AppServer GUI</div>
+				<div id="banner">&nbsp;</div>
+			</div> <!--end header-->
+			
+			<div id="container">
+				<div id="menu-left">&nbsp;this is the menu-left</div>
+					
+				<div id="stage">
+					<h1><?php echo $content  ?></h1>
+					<div id="modal">
+						<div id="content">
+					</div>
+				</div>
+			</div> <!--end container-->
+						
+			<div id="footer">
+				<div class="content">
+					<span style="font-size:smaller;">&copy;&nbsp;2019</span>
+				</div> <!--end content-->
+			</div> <!--end footer-->
+		</div> <!--end page -->
 
-        <button>Download .zip of all event materials</button>
-    </div>
-
-    <hr>
-    <div class = "table-container2", id="table-container2">
-
-	</div>
-		
-		
-		<div id="footer">
-		 	<div class="content">
-		 		<span style="font-size:smaller;">&copy;&nbsp;2019</span>
-		 	</div>
 		</div>
 	</body>
 	
