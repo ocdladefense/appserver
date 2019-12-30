b2e4e3d80be62cadd31c7f4125cfc659fef74bac
Jose Bernal <jbernal.web.dev@gmail.com>
2019-12-30 02:22:59 +0000

    Refactor template file.

--- content/themes/default/webconsole.tpl.php
+++ content/themes/default/webconsole.tpl.php
@@ -12,12 +12,9 @@
 	<head>
 		<meta charset="utf-8" />
 		<title>AppServer Client</title>
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link href="/modules/webconsole/assets/css/ux.css" type="text/css" rel="stylesheet" />
-		<link href="/modules/webconsole/assets/css/materials.css" type="text/css" rel="stylesheet" />
-		<link href="/modules/webconsole/assets/css/KeyboardManager.css" type="text/css" rel="stylesheet" />
-		<link href="/modules/webconsole/assets/modules/note/styles.css" type="text/css" rel="stylesheet" />
-		<link href="/modules/webconsole/assets/css/siteStatus.css" type="text/css" rel="stylesheet" />
+
+		<?php echo $styles; ?>
+		
 		<script type="text/javascript">
 			window.appModules = {};
 			
@@ -28,20 +25,46 @@
 			}
 			
 			function getModule(name){
+				if(!appModules[name]) throw new Error("Module "+name+" hasn't been loaded.");
 				return appModules[name];
 			}
 		</script>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
 		</script>
-    	 <?php echo $scripts; ?> 
+		
+		<?php echo $scripts; ?> 
 	</head>
 	<body>
 
 		<div id="page">
 			<div id="header">
-				<div id="logo">AppServer GUI</div>
-				<div id="banner">&nbsp;</div>
+				<div class="header-content">
+					<div id="logo">AppServer GUI</div>
+					<div id="banner">&nbsp;</div>
+				</div>
 			</div> <!--end header-->
+			
+			<div id="doc-header">
+				<div id="doc-nav">
+			
+					<div class="doc-nav" id="doc-nav-left">
+					 Some content here
+					</div>
+				
+					<div class="doc-nav" id="doc-nav-content">
+						<span id="doc-nave-open" data-controller-for="doc-nav" class="collapsible-controller arrow-right"></span>
+						<div class="doc-title active"><a href="#" data-doc-id="1" data-route="load-doc">Chapter 1: Precharge Represenation</a></div>
+						<div class="doc-title"><a href="#" data-doc-id="2" data-route="load-doc">Chapter 2: Getting Your Client Out: Bail and Release</a></div>
+						<div class="doc-title"><a href="#" data-doc-id="3" data-route="load-doc">Chapter 3: Themes in Sex Crime Defense: Developing, Polishing and Preserving the Stories of Your Defense at Every Stage of Your Case</a></div>
+						<div class="doc-title"><a href="#" data-doc-id="4" data-route="load-doc">Chapter 4: Preparing the Case: Discovery, Investigation and Obtaining Evidence</a></div>
+					</div>
+
+					<div class="doc-nav" id="doc-nav-right">
+						Some content here
+					</div>
+				
+				</div>
+			</div>
 			
 			<div id="container">
 				<div id="container-left">
@@ -51,10 +74,14 @@
 					
 				<div id="stage" class="<?php echo $defaultStageClass; ?>">
 
-					<div id="stage-content"><?php echo $content; ?></div>
-					
-					
-
+					<div id="stage-content">
+						<author>Steve Sherlag</author>
+						<doc>
+							<title>Chapter 4</title>
+							<author>Steve Eberlein</author>
+							<author>Seth White</author>
+						</doc>
+						<?php echo $content; ?></div>
 				</div>
 			</div> <!--end container-->
 						
@@ -104,8 +131,10 @@
 		</script>
 		
 		<!--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		-->
+		<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		-->
 
 	</body>
 
