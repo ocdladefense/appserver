ad8307bd2281339128d8bdb1505b0b51846e5349
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2020-02-24 14:25:30 -0800

    result object

--- includes/Database/MysqlDatabase.php
+++ includes/Database/MysqlDatabase.php
@@ -19,9 +19,8 @@ class MysqlDatabase{
     }
 
     function connect(){
-                // Create connection
-                //dbhost,dnuser,dbpassword,dbname
-        $this->connection = new mysqli(HOST_NAME,USER_NAME,USER_PASSWORD,DATABASE_NAME);
+        //Create connection
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
         // Check connection
         if ($this->connection->connect_error) {
@@ -34,44 +33,93 @@ class MysqlDatabase{
         $escaped = $this->prepareData($values);
         $formatted = implode("','",$escaped);
         $columnNames = implode(", ",$columns);
-        $query = "INSERT INTO $tableName ($columnNames) VALUES ('$formatted')";
+        $sql = "INSERT INTO $tableName ($columnNames) VALUES ('$formatted')";
 
-        $this->doQuery($query);
+        $result = $this->doQuery($sql);
+        return $result->status;
     }
 
     function doSelect($json){
         $rows = array();
 
-        $sql = selectClause().whereClause($json);
-        $result = $this->connection->query($sql);
+        $sql = $this->selectClause().$this->whereClause($json);
+        //$sql = "SELECT * FROM over there";
+        $result = $this->doQuery($sql);
     
-        if ($result != null) {
-            if($result->num_rows > 0){
-                while($row = $result->fetch_assoc()){
+        if ($result->data != null) {
+            if($result->data->num_rows > 0){
+                while($row = $result->data->fetch_assoc()){
                     $rows[] = $row;
                 }
             }
             return $rows;
-        } else {
-            echo "<br><strong>ERROR RETRIEVING RECORD: <br>" . $queryObj . "<br>" . $this->connection->error . "<br></strong>";
+        } else if($result->hasError) {
+            return $result->status;
         }
+        throw new DbException("Something unexpected happened....not quite sure what it is yet though.");
     }
 
-    //move select and where into the class
-    //doquery should return a result
-    //save the echos to a status
-    //if therer when running the query set hasError to true
     function doQuery($query){
-        if ($this->connection->query($query) === TRUE) {
-            echo "<br><strong>New record created successfully<br></strong>";
-        } else {
-            echo "<br><strong>ERROR CREATING RECORD: <br>" . $query . "<br>" . $this->connection->error . "<br></strong>";
+        $conn = $this->connection->query($query);
+        $result = new StdClass();
+        $result->hasError = false;
+
+        if(strpos($query,"INSERT") !== false){
+            if ($conn === TRUE) {
+                $result->status = "<br><strong>New record created successfully<br></strong>";
+            } else {
+                $result->hasError = true;
+                $result->status = "<br><strong>ERROR CREATING RECORD: <br>" . $query . "<br>" . $conn->error . "<br></strong>";
+            }
         }
+
+        if(strpos($query,"SELECT") !== false){
+            if($conn != null && $conn->num_rows > 0){
+                $result->data = $conn;
+            }
+            else{
+                $result->hasError = true;
+                $result->status = "<br><strong>ERROR RETRIEVING RECORD: <br>" . $query . "<br>" . $conn->error . "<br></strong>";
+            }
+        }
+
+        return $result;
     }
     
     function close(){
         $this->connection->close();
     }
+
+    //Additional Methods
+
+    function selectClause(){
+        $tableName = "car";
+        $selectFields = array();
+        return "SELECT * FROM $tableName";
+    }
+    
+    function whereClause($conditions){
+        $where = "";  // Prepare to build a SQL WHERE clause
+        $tmp = array();
+        
+         foreach($conditions as $c){
+             $field = $c->field;
+             $op = $c->op;
+             $value = $c->value;
+     
+             if(is_int($value)){
+                 $tmp []= sprintf("%s %s %d",$field,$op,$value);
+             } else if($op == 'LIKE'){
+                 $tmp [] = sprintf("%s %s '%%%s%%'",$field,$op,$value);
+             } else {
+                 $tmp [] = sprintf("%s %s '%s'",$field,$op,$value);
+             }
+         }
+     
+         $where .= " WHERE ".implode(' AND ',$tmp);
+     
+         return $where;
+     }
 }
 
 function insert($obj){
@@ -82,7 +130,7 @@ function insert($obj){
     $tableName = get_class($obj);
 
     $db = new MysqlDatabase();
-	$db->insert($tableName,$columns,$values);
+	return $db->insert($tableName,$columns,$values);
 }
 
 function select($json){
@@ -90,34 +138,7 @@ function select($json){
     return $db->doSelect($json);
 }
 
-function selectClause(){
-	$tableName = "car";
-	$selectFields = array();
-	return "SELECT * FROM $tableName";
-}
 
-function whereClause($conditions){
-    $where = "";  // Prepare to build a SQL WHERE clause
-    $tmp = array();
-    
-     foreach($conditions as $c){
-         $field = $c->field;
-         $op = $c->op;
-         $value = $c->value;
- 
-         if(is_int($value)){
-             $tmp []= sprintf("%s %s %d",$field,$op,$value);
-         } else if($op == 'LIKE'){
-             $tmp [] = sprintf("%s %s '%%%s%%'",$field,$op,$value);
-         } else {
-             $tmp [] = sprintf("%s %s '%s'",$field,$op,$value);
-         }
-     }
- 
-     $where .= " WHERE ".implode(' AND ',$tmp);
- 
-     return $where;
- }
  
 
 
