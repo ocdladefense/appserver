3c68273b7cee91e4e34a9142ca3e8d74d054875b
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2020-03-10 13:26:34 -0700

    insert

--- includes/Database/MysqlDatabase.php
+++ includes/Database/MysqlDatabase.php
@@ -24,7 +24,7 @@ class MysqlDatabase{
 
         if($result !== true){
         
-            throw new DbException("Error inserting data in sql query  " . $sql . $this->connection->error);
+            throw new DbException("Error inserting data.  " . $this->connection->error);
         }
 
         $count = mysqli_affected_rows($this->connection);
