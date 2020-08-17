6edc8a74bf6fc0a89bd5559335b1fdeaeceaa872
Jose Bernal <jbernal.web.dev@gmail.com>
2020-08-16 22:29:10 -0700

    Adjust request headers; add support for file upoads.

--- includes/Http/HttpConstants.php
+++ includes/Http/HttpConstants.php
@@ -20,4 +20,8 @@ const MIME_APPLICATION_JSON = "application/json";// charset=utf-8";
 
 const MIME_TEXT_JAVASCRIPT = "text/javascript";
 
+const MIME_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+const MIME_MULTIPART_FORM_DATA = "multipart/form-data";
+
 const CONTENT_TYPE_APPLICATION_X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded";
