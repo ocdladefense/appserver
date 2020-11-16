18a7b5bd67bc999ed10a72a58651890a175fd3d3
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2020-11-16 11:17:03 -0800

    Rename the login credential files.

--- index.php
+++ index.php
@@ -10,13 +10,17 @@ use Http\HttpHeader;
 use Http\HttpRequest;
 use Http\Http;
 
+$loginType = "admin"; //Other option is community login.
+
 // curl https://login.salesforce.com/services/Soap/u/50.0 -H "Content-Type: text/xml; charset=UTF-8" -H "SOAPAction: login" -d @login-community.txt | xmllint --format -
 	$url = "https://login.salesforce.com/services/Soap/u/50.0";
 	$request = new HttpRequest($url);
 
+	
+
 	$request->addHeader(new HttpHeader("Content-Type", "text/xml; charset=UTF-8"));
 	$request->addHeader(new HttpHeader("SOAPAction", "login"));
-	$pathToLogin = $dir."/config/login-community.xml";
+	$pathToLogin = $loginType == "admin" ? $dir."/config/soap-login-admin-user.xml" : $dir."/config/soap-login-community-user.xml";
 	$load = file_get_contents($pathToLogin);
 	$request->setBody($load);
 	
