6c052f15bbc9e7e5e66fcd0cdcb3b1b306a6cdc8
Jose Bernal <jbernal.web.dev@gmail.com>
2020-12-27 17:28:31 -0800

    Get user id.

--- includes/User.php
+++ includes/User.php
@@ -4,24 +4,28 @@ class User {
 
 
 	public static function add_session_data($saml = array()) {
-		$userId = $saml["userId"][0];
-		$username = $saml["username"][0];
-		$email = $saml["email"][0];
-		$isPortalUser = $saml["is_portal_user"][0];
+		$userId 				= $saml["userId"][0];
+		$username 			= $saml["username"][0];
+		$email 					= $saml["email"][0];
+		$isPortalUser 	= $saml["is_portal_user"][0];
 	
-		$_SESSION["userId"] = $userId;
-		$_SESSION["username"] = $username;
-		$_SESSION["email"] = $email;
+		$_SESSION["userId"] 			= $userId;
+		$_SESSION["username"] 		= $username;
+		$_SESSION["email"] 				= $email;
 		$_SESSION["isPortalUser"] = $isPortalUser;
 	}
 
 
 	public static function is_authenticated() {
-		return isset($_SESSION["userId"]);
+		return defined("ADMIN_USER") && ADMIN_USER === true || isset($_SESSION["userId"]);
 	}
 
 
-
+	// Assume that uid of 1 is the super-user for now.
+	//  Other ids will be unique for each individual logged-in member.
+	public static function getId() {
+		return defined("ADMIN_USER") && ADMIN_USER === true ? 1 : $_SESSION["userId"];
+	}
 
 
 
