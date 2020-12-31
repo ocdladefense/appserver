c6657e2adc4f488b748ef8296645bff6cb2b9bce
jeffw773 <49732785+jeffw773@users.noreply.github.com>
2020-12-31 13:35:09 -0800

    Bug needs to be squashed >:(

--- includes/Database/salesforce/Database.php
+++ includes/Database/salesforce/Database.php
@@ -109,11 +109,24 @@ class Database {
 
 		//INSERT INTO Media__c (ResourceId__c, Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c) VALUES ('YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02')
 
+		$newquery = trim($query, "()");
+
+		print($newquery);
+
+		exit;
+		print(" ");
+		print("Statement________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
 
 		$statement = explode("INSERT INTO", $query);
 
 		//$statement[0] is empty
 		//$statement[1] is everything to the right of INSERT INTO
+
+		print_r($statement);
+		print(" ");
+		print("ProtoKeyValues__________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
 
 		//Media__c(ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c)VALUES('YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02'),(THESE ARE MY OTHER VALUES)
 		//if we explode by space... array = {"Media__c", "(ResourceId__c,", "Name,")}
@@ -126,52 +139,110 @@ class Database {
 
 		
 
-		$protoKeyValues = explode($statement[1], "VALUES");
+		$protoKeyValues = explode("VALUES", $statement[1]);
 		
-		$valueString = preg_replace($protoKeyValues[1], ")\s*,\s*(","),(");  //standardizing multiple records
-		//look up preg_replace on php.net!
+		print_r($protoKeyValues);
+		print(" ");
+		print("valueString______________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+		
 
-		$values = explode(protoKeyValues[1], "),(");
+		$valueString = preg_replace("/\)\s*,\s*\(/", "),(", $protoKeyValues[1]);  //standardizing multiple records
+		//look up preg_replace on php.net!
+		//IF THERE IS NO PATTERN THEN IT WILL RETURN NULL!!
+
+		print($valueString);
+		print(" ");
+		print("values___________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+		
+
+		$values = explode("),(", $valueString);
 		//Thing I want to use for inital set of values should be taking protoKeyValues at index of [1]
 
-
-
-
-
-		//$protoKeyValues[0] is Media__c(ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c)
-		//$protoKeyValues[1] is ('YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02')
-
-		$SObjectKey = explode ($protoKeyValues[0], "(");
-
-		//$SObjectKey[0] is Media__c
-		//$SObjectKey[1] is ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c)
-
-		//$SObjectKey[1] are keys
-		//$protoKeyValues[1] are values
-		$trimKeys = trim($SObjectKey[1], ")");
-		$trimValues = trim($keyValues[1], "()");
-
-		//$trimKeys is  ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c
-		//$trimValues is 'YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02'
-
-
-		$keys = explode($trimKeys, ',');
-		$values = explode($trimValues, ",");
-
-		//$keys is now {"ResourceId__c", "Name", "Speakers__c", "Description__c", "IsPublic__c", "Published__c", "Date__c"}
-		//$values is now {"YtoqDF8EnsA", 'My Home Video 1', "Josh Cathey", "Josh gives a lecture", true, true, "2020-02-02"}
+		print_r($values);
+		print(" ");
+		print("SObjectKey_______________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
 		
 		$getRidofQuotes = function ($item) {
 			return trim($item, "\"'");
 		};
 
+		$getRidofParen = function ($item) {
+			return trim($item, "()");
+		};
+
+		//$protoKeyValues[0] is Media__c(ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c)
+		//$protoKeyValues[1] is ('YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02')
+
+		$SObjectKey = explode ("(", $protoKeyValues[0]);
+
+		//$SObjectKey[0] is Media__c
+		//$SObjectKey[1] is ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c)
+
+		print_r($SObjectKey);
+		print(" ");
+		print("trimKeys_________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
+		//$SObjectKey[1] are keys
+		//$values are values
+		print("<h2>Setting simple string</h2>");
+		$simpleString = $SObjectKey[1];
+		print($simpleString);
+		$trimKeys = trim($simpleString, ')');
+		print("</br>");
+		print($trimKeys);
+		print("</br>");
+		exit;
+		$trimmedValues = array_map($getRidofParen, $values);
+
+		print($trimKeys);
+		print(" ");
+		print("trimmedValues_______________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
+		print_r($trimmedValues);
+		print(" ");
+		print("keys_____________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
+		//$trimKeys is  ResourceId__c,Name, Speakers__c, Description__c, IsPublic__c, Published__c, Date__c
+		//$trimValues is 'YtoqDF8EnsA', 'name', 'speakers', 'description', true, true, '2020-02-02'
+
+
+		$keys = explode(',', $trimKeys);
+		$values = explode(",", $trimValues,);
+
+		//$keys is now {"ResourceId__c", "Name", "Speakers__c", "Description__c", "IsPublic__c", "Published__c", "Date__c"}
+		//$values is now {"YtoqDF8EnsA", 'My Home Video 1', "Josh Cathey", "Josh gives a lecture", true, true, "2020-02-02"}
+		
+		print_r($keys);
+		print(" ");
+		print("values___________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
+		print_r($values);
+		print(" ");
+		print("_________________________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
+
+		
+		print(" ");
+		print("trimmedValues_______________________________________________________________________________________________________________________________________________________________________________");
+		print(" ");
+
 		//array_map ( callable|null $callback , array $array , array ...$arrays )?
 		$trimmedValues = array_map($getRidofQuotes, $values);
+
+		print_r($trimmedValues);
 
 		$record = array_combine($keys ,$trimmedValues);
 		//want '' coming in input, but want to strip them out for output using trim(value, "'") using a foreach
 
-		console.log($record);
+		
 		//array_map ( callable|null $callback , array $array , array ...$arrays )
 
 
