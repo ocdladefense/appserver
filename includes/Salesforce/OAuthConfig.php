25934e20669b060d096de0def882bf5e87b18b09
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-04-06 16:32:17 -0700

    new config files

--- includes/Salesforce/OAuthConfig.php
+++ includes/Salesforce/OAuthConfig.php
@@ -2,49 +2,140 @@
 
 namespace Salesforce;
 
-class OAuthConfig extends Config{
+class OAuthConfig {
 
-    private $username;
+    private $config;
 
-    private $password;
+    private $name;
 
-    private $securityToken;
+    private $default;
+
+    private $sandbox;
+
+    private $clientId;
+
+    private $clientSecret;
+    
+    private $authorizationUrl;
+
+    private $authorizationRedirectUrl;
 
     private $callbackUrl;
+
+    private $tokenUrl;
 
 
     public function __construct($config){
 
-        parent::__construct($config);
+        $this->config = $config;
+        $this->name = $config["name"];
+        $this->default = $config["default"];
+        $this->sandbox = $config["sandbox"];
+        $this->clientId = $config["client_id"];
+        $this->clientSecret = $config["client_secret"];
+        $this->tokenUrl = $config["token_url"];
 
-        $this->username = $config["username"];
-        $this->password = $config["password"];
-        $this->securityToken = $config["security_token"];
-        $this->callback = $config["callback_url"];
+        $this->callbackUrl = $config["callback_url"];
+
     }
 
-    public function getUsername(){
+    public function getFlowConfig($flow = "usernamepassword"){
 
-        return $this->username;
+        $tmp = array(
+            "username" => $this->config["username"],
+            "password" => $this->config["password"],
+            "securityToken" => $this->config["security_token"]
+        );
+
+        return new OAuthFlowConfig($tmp);
+
     }
 
-    public function getPassword(){
+    public static function fromFlow($config, $flow){
 
-        return $this->password;
+        $oauthConfig = new self($config);
+        
+        return $oauthConfig->newFromFlow($oauthConfig, $flow);
     }
 
-    public function getSecurityToken(){
+    public function getName(){
 
-        return $this->securityToken;
+        return $this->name;
     }
 
-    public function getPasswordAndSecurityToken(){
+    public function getPasswordWithSecurityToken(){
 
         return $this->password . $this->securityToken;
     }
-    
+
+    public function getAuthorizationEndpoint(){
+
+        return $this->authorizationEndpoint;
+    }
+
+    public function getAuthorizationRedirect(){
+
+        return $this->authorizationRedirect;
+    }
+
     public function getCallbackUrl(){
 
         return $this->callbackUrl;
+    }
+
+    public function getClientId(){
+
+        return $this->clientId;
+    }
+
+    public function getClientSecret(){
+
+        return $this->clientSecret;
+    }
+
+    public function getTokenUrl(){
+
+        return $this->tokenUrl;
+    }
+
+    public function getConfig(){
+
+        return $this->config;
+    }
+
+    public function newFromFlow($oauthConfig, $flow){
+
+
+
+        switch($flow) {
+
+            case "usernamePassword":
+                return $this->usernamePasswordFlow($oauthConfig, $flowConfig);
+                break;
+            case "webserver":
+                return $this->webserverFlow($oauthConfig, $flowConfig);
+                break;
+            default:
+                throw new \Exception("OAUTH_CONFIG_ERROR:   No functionality for given config.");
+        }
+
+    }
+
+    public function usernamePasswordFlow($oauthConfig, $flowConfig){
+
+        $oauthConfig->username = $flowConfig["username"];
+        $oauthConfig->password = $flowConfig["password"];
+        $oauthConfig->securityToken = $flowConfig["security_token"];
+        $oauthConfig->callbackUrl = $flowConfig["callback_url"];
+
+        return $oauthConfig;
+    }
+
+    public function webServerFlow($oauthConfig, $flowConfig){
+
+        $oauthConfig->authorizationUrl = $flowConfig["auth_url"];
+        $oauthConfig->authorizationRedirectUrl = $flowConfig["auth_redirect_url"];
+        $oauthConfig->callbackUrl = $flowConfig["final_redirect_url"];
+        return $oauthConfig;
     }
 }
