5f9413b2adcb460c15a98df1bf419b451bdc6366
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-08-03 14:57:53 -0700

    New session and cookie settings in a settings.php file.

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,7 @@
 <?php
 
 
-	if(!defined("BASE_PATH")) {
-		define("BASE_PATH",__DIR__);
-	}
+if(!defined("BASE_PATH")) define("BASE_PATH", __DIR__);
 
-
-	require(BASE_PATH . '/includes/autoload.php');
+require(BASE_PATH . "/includes/settings.php");
+require(BASE_PATH . "/includes/autoload.php");
