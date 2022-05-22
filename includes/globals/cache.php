de6920e39ff6e79858c48076f809710b3f3b8df4
Jose Bernal <jbernal.web.dev@gmail.com>
2022-05-22 15:42:54 -0700

    Add application cache.

---
+++ includes/globals/cache.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+
+function cache_set($key, $val) {
+	$val = var_export($val, true);
+	// HHVM fails at __set_state, so just use object cast for now
+	$val = str_replace('stdClass::__set_state', '(object)', $val);
+	// Write to temp file first to ensure atomicity
+	$tmp = CACHE_DIR ."/$key." . uniqid('', true) . '.tmp';
+	file_put_contents($tmp, '<?php $val = ' . $val . ';', LOCK_EX);
+	rename($tmp, CACHE_DIR."/$key");
+ }
+
+
+ function cache_get($key) {
+    @include CACHE_DIR."/$key";
+    return isset($val) ? $val : false;
+}
